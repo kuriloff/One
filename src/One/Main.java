96adf10c91f82fcddb75f40ea15ce860d413874d
kuriloff <143285623+kuriloff@users.noreply.github.com>
2023-09-24 16:43:20 +0400

    1

--- src/One/Main.java
+++ src/One/Main.java
@@ -7,10 +7,34 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         String mem;
-        MembershipManagement mm;
-        FileHandler fh;
-        LinkedList<Member> members;
-        int choice;
+        MembershipManagement mm = new MembershipManagement();
+        FileHandler fh = new FileHandler();
+        LinkedList<Member> members = fh.readFile(); //        new LinkedList<Member>();
+        for (Member m: members
+             ) {
+            System.out.println(m.toString());
+        }
+
+        //fh.readFile();
+        int choice = 0;
+        for(;;){
+            choice = mm.getChoice();
+            if(choice == 0) break;
+            else if(choice == -1) break;
+            else if(choice == 1){
+                mem = mm.addMembers(members);
+                fh.appendFile(mem);
+            }
+            else if(choice == 2) {
+                mm.removeMember(members);
+                fh.overwriteFile(members);
+            }
+            else if(choice == 3) {
+                mm.printMemberInfo(members);
+            }
+            else{};
+        }
+
 
     }
 }
